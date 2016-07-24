572008463d7375c7370f55ec33754beac7493ade
Edwin Rivera <edwin.rivera@outlook.com>
2016-07-24 13:30:14 -0400

    added mock products to database

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -10,3 +10,33 @@ CREATE TABLE products(
   StockQuantity INT NULL,
   PRIMARY KEY(ItemID)
 );
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Mechanical Keyboard", "Computer Peripherals", 35.95, 100);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Mouse", "Computer Peripherals", 25.95, 100);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Nvidia GTX 1080", "Computer Parts", 699.95, 50);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Nvidia GTX 1070", "Computer Parts", 299.95, 75);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("ASUS G-Sync Monitor", "Computer Peripherals", 749.95, 35);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Overwatch", "Computer Games", 59.99, 40);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Counter-Strike: Global Offensive", "Computer Games", 24.99, 40);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Sonic-06", "Computer Games", 4.99, 1000);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Rolercoaster Tycoon 2", "Computer Games", 7.99, 500);
+
+INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
+VALUES ("Minecraft", "Computer Games", 89.99, 1);
