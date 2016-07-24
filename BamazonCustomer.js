71bc1296d6c8bd28c1d36713a222605bbc510139
Edwin Rivera <edwin.rivera@outlook.com>
2016-07-24 14:12:04 -0400

    created initial display of items from db

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -9,7 +9,17 @@ var connection = mysql.
     password: 't3st3r',
     database: 'bamazon_db'
   })
-  connection.connect(function(err){
-    if(err) throw err;
-    console.log('Connected as: ' + connection.threadId);
-  })
+  
+connection.connect(function(err){
+  if(err) throw err;
+  console.log('Connected as ID: ' + connection.threadId);
+  console.log("\nWelcome to Bamazon! Today's hot items are...\n")
+})
+
+connection.query('SELECT * FROM `products`', function(err, results, fields){
+  for (var i=0; i < results.length; i++){
+    console.log(results[i].ItemID +" "+ results[i].ProductName +" $"+ results[i].Price);
+    console.log("----------");
+  }
+});
+
